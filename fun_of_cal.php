880005dcf79080dfb4e2e12448c9a8683ebab905
myselfmuneb <muneebbutt444@gmail.com>
2020-05-14 21:22:09 +0500

    assignmet

---
+++ fun_of_cal.php
@@ -0,0 +1,21 @@
+<?php
+
+function add($num1 , $num2){
+		$result= $num1 + $num2;
+		echo "Here is the Answer of add: " . $result;
+	}
+
+function sub($num1 , $num2){
+		$result= $num1 - $num2;
+		echo "Here is the Answer of Subtract: " . $result;
+	}
+
+function multi($num1 , $num2){
+		$result= $num1 * $num2;
+		echo "Here is the Answer of Multiply: " . $result;
+	}
+
+function div($num1 , $num2){
+		$result= $num1 / $num2;
+		echo "Here is the Answer of Divide: " . $result;
+	}		
