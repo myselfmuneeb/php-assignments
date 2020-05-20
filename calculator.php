ece62909a02269a8ccdda357a903190726f416d2
myselfmuneb <muneebbutt444@gmail.com>
2020-05-20 07:24:09 +0500

    Calculator

--- calculator.php
+++ calculator.php
@@ -4,25 +4,27 @@
 
 	$num1 = 15;
 	$num2 = 10;
-	add($num1 , $num2);
+	echo "The Sum of values is : " . add($num1 , $num2);
 
 	echo "<br>";
 
 	$num3 = 30;
 	$num4 = 10;
-	sub($num3 , $num4);
+	echo "The Subtract of values is : " . sub($num3 , $num4);
 
 	echo "<br>";
 
 	$num5 = 10;
 	$num6 = 10;
-	multi($num5 , $num6);
+	echo "The mMltiply of values is : " . multi($num5 , $num6);
 
 	echo "<br>";
 
 	$num7 = 50;
 	$num8 = 10;
-	div($num7 , $num8);
+	echo "The Dvied of values is : " . div($num7 , $num8);
+
+
 
 
 ?>
