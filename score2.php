26a6da3f49950d81af3fc9d78814a2e72374cf82
myselfmuneb <muneebbutt444@gmail.com>
2020-05-26 16:32:00 +0500

    score

--- score2.php
+++ score2.php
@@ -1,10 +1,10 @@
 <?php
 
 $total_score = 100;
-$score =69;
+$score =99;
 $score_per = $score /$total_score * 100;
 
-if ($score<$total_score) {
+if ($score<=$total_score && $score>=0) {
 	if ($score_per >= 90) {
 		echo "you score is : " . $score . "<br>";
 		echo "You are pass. Grade: A+";
@@ -25,10 +25,12 @@ if ($score<$total_score) {
 		echo "you score is : " . $score . "<br>";
 		echo "You are pass. Grade: D";
 	}
-	elseif ($score_per < 0) {
-		echo "Given score $score is invalid";
+	elseif ($score_per < 50){
+		echo "you score is : " . $score . "<br>";
+		echo "You are Fail. Grade: F";
 	}
-}else{ echo "Total marks are " . $total_score . " so score should be less than total marks!";}
+	
+}else{ echo "Given score $score is invalid";}
 
 
 
