872adacc1c4b9fba98b3670371e2c34e7d47afc5
myselfmuneb <muneebbutt444@gmail.com>
2020-05-28 09:28:41 +0500

    table

---
+++ table.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "FOR LOOP: <br>";
+$table = 2;
+$num = 1;
+for ($table; $num <= 10 ; $num++) { 
+	echo $table . " X " . $num . " = " . $table*$num . "<br>" ;
+}
+
+echo "WHILE LOOP :<br>";
+
+$table2 = 3;
+$num2 = 1;
+while ($num2 <= 10) {
+	echo $table2 . " X " . $num2 . " = " . $table2*$num2 . "<br>" ;
+	$num2++;
+}
+
+echo "DO WHILE LOOP :<br>";
+
+$table3 = 4;
+$num3 = 1;
+
+do{
+	echo $table3 . " X " . $num3 . " = " . $table3*$num3 . "<br>" ;
+	$num3++;
+}while($num3 <= 10);
+
+?>
