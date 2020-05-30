e9cd38989781d92ac62d71adcd3e75dc0ae1ecb2
myselfmuneb <muneebbutt444@gmail.com>
2020-05-30 14:10:27 +0500

    assignment

---
+++ 2darray.php
@@ -0,0 +1,21 @@
+<?php
+
+$laptop = array(
+
+	array("Name :" , " Age :" , " Married / single :"),
+	array("Ali " , 24 , " single"),
+	array("Muneeb " , 25 , " single")
+
+);
+
+foreach ($laptop as $keys => $value) {
+	echo "<br>";
+	foreach ($value as $key => $values) {
+		echo $keys . " " . $key . " " . $values;
+	}
+	echo "<br>";
+}
+
+//echo $laptop[1][0];
+
+?>
---
+++ maxvalue.php
@@ -0,0 +1,13 @@
+<?php
+
+$values = array(30 ,1 , 2 , 3 , 4 , 5 , 6 , 20 );
+$maxvalue = $values[0];
+
+for ($i=0; $i <count($values) ; $i++) { 
+	
+	if ($maxvalue<$values[$i]) {
+		$maxvalue = $values[$i];
+	}
+}
+echo $maxvalue;
+?>
