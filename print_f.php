8b062e8e318dd2bf03e86977f3d379fd94a00f8f
myselfmuneb <muneebbutt444@gmail.com>
2020-06-14 04:15:04 +0500

    assign

---
+++ print_f.php
@@ -0,0 +1,13 @@
+<?php
+
+$price = 20000;
+$price2 = 5000;
+//print "<pre>";
+printf("hi i have %'*9d and i will give you %d <br>" , $price , $price2);
+$num = 500;
+printf("the desimal of given number is = %b <br>" , $num);
+//print "</pre>";
+
+printf("%f %.2f <br>" , 24 , 24.23);
+echo sprintf("%'*8.5s" , "Hello World");
+?>
